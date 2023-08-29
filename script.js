e4049b8b96c306559b30b06f024994bc5c975d85
ludiaz978 <142175671+ludiaz978@users.noreply.github.com>
2023-08-29 14:24:53 -0700

    project-in progress

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //for java script code
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+   
+    var task = input.value;
+    input.value = '';
+   
+    addTaskToList(task);
+    localStorage.setItem(task, task);
+   });
+   
+var form = document.getElementById('todo-input');
+//accessing from html file
+var form = document.getElementById('todo-list');
+window.addEventListener('load', function) {
+    console.log('page has loaded');
+}
