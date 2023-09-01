665d3c10388c9a9d16df51521bf7b8bb4bb53f3a
ludiaz978 <142175671+ludiaz978@users.noreply.github.com>
2023-08-31 19:29:38 -0700

    html final

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //for java script code
+var form = document.getElementById('todo-form');
+//accessing from html file
+var list= document.getElementById('todo-list');
+var input=document.getElementById('todo-input');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -8,11 +12,35 @@ form.addEventListener('submit', function(event) {
    
     addTaskToList(task);
     localStorage.setItem(task, task);
-   });
-   
-var form = document.getElementById('todo-input');
-//accessing from html file
-var form = document.getElementById('todo-list');
-window.addEventListener('load', function) {
+});
+
+window.addEventListener('load', function() {
     console.log('page has loaded');
+
+    for (var i = 0; i < localStorage.length; i++) {
+        addTaskToList(localStorage.key(i));
+      }
+});
+
+function addTaskToList(task) {
+    var listItem=document.createElement('li');
+    listItem.textContent = task;
 }
+
+listItem.addEventListener('click', function() {
+    listItem.remove();
+    localStorage.removeItem(task);
+  });
+  function addTaskToList(task) {
+    var listItem = document.createElement('li');
+    listItem.textContent = task;
+  
+    listItem.addEventListener('click', function() {
+      listItem.remove();
+      localStorage.removeItem(task);
+    });
+  
+    list.appendChild(listItem);
+  }
+  
+     
